7a2b1704861b5442467151e063b93863bd398a70
Klimov-V <klimov_vitaliy@icloud.com>
2020-07-17 21:58:30 +0300

    added pagination buttons, added swipert to products section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,20 @@
 (() => {
     'use strict'
 
+    var productsSlider = new Swiper('#productsSlider', {
+        loop: true,
+        slidesPerView: 1,
+        centeredSlides: true,
+        spaceBetween: 30,
+      
+        
+        
+        navigation: {
+          nextEl: '.slider__arrow--right',
+          prevEl: '.slider__arrow--left',
+        },
+    })
     
     
 })();
+
