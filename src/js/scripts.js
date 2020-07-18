05bd8784d9412a039732e13a71c271b4679f5c9e
Klimov-V <klimov_vitaliy@icloud.com>
2020-07-18 13:05:57 +0300

    active links in header menu with scrolling

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,21 +7,12 @@ headroom.init();
 
 
 
-let scrinWidth = window.screen.width;
-let slidesValue;
-let isCentered = scrinWidth < 992 ? false : true;
+let scrinWidth = window.screen.width,
+    slidesValue = scrinWidth < 578 ? 1 : scrinWidth < 992 ? 2 : 3,
+    isCentered = scrinWidth < 992 ? false : true,
+    html = document.querySelector('body');
 
-let checkWidth = () => {
-    return scrinWidth < 578 ? 1 : scrinWidth < 992 ? 2 : 3;
-}
 
-slidesValue = checkWidth();
-
-document.querySelector('body').addEventListener('resize', () => {
-    console.log(slidesValue);
-    checkWidth()
-    console.log(slidesValue);
-});
 
 var productsSlider = new Swiper('#productsSlider', {
     loop: true,
@@ -35,3 +26,36 @@ var productsSlider = new Swiper('#productsSlider', {
         prevEl: '.slider__arrow--left',
     },
 })
+
+//  Этот закоментированный код должен динамически менять значение slidesPerView в слайдере
+//     и, соответственно, менять к-во отображаемых единовременно слайдов. Значение меняется, 
+//     но изменения в отображении слайдера появляются только после перезагрузки, а это и так происходит без данного кода.
+
+// window.addEventListener('scroll', () => {
+//     scrinWidth = window.screen.width;
+//     slidesValue = scrinWidth < 578 ? 1 : scrinWidth < 992 ? 2 : 3;
+//     console.log(slidesValue);
+//     console.log(scrinWidth);
+//     productsSlider.slidesPerView = slidesValue;
+//     console.log(`slidesPerView - ${productsSlider.slidesPerView}`);
+// });
+
+
+window.addEventListener('scroll', () => {
+    let activeLink = document.querySelector(".navigation__link--active");
+    // activeLink && activeLink.classList.remove("navigation__link--active");
+    let counter, sections = document.querySelectorAll("section");
+    for (counter = 0; counter < sections.length; counter++) {
+      let section = sections[counter];
+      if (section.getBoundingClientRect().top > 1) break;
+      
+    }
+    if (--counter >= 0) {
+        
+      let index = sections[counter] && sections[counter].getAttribute("id"),
+          link = document.querySelector('a[href="#' + index + '"]');
+
+          activeLink && activeLink.classList.remove("navigation__link--active");
+      index && link && link.classList.add("navigation__link--active");
+    }
+});
